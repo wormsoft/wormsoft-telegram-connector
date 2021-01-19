155f4c65d906848e1ce940656c9e54d55b7b0035
Anton Morev <shikto@mail.ru>
2021-01-19 16:50:38 +0300

    Non required fields

--- src/lib/config/index.ts
+++ src/lib/config/index.ts
@@ -2,12 +2,10 @@ import * as TT from 'telegram-typings';
 import { MainTelegramBrain } from '../brain';
 
 export interface SimpleConfig {
-    adminSupportChat: number;
     token: string;
-    appName: string;
-    botInfo: TT.User;
-    brainInit: (brain: MainTelegramBrain) => void;
-    appConfig: any
+    botInfo?: TT.User;
+    brainInit?: (brain: MainTelegramBrain) => void;
+    appConfig?: any
 }
 
 export interface WSTelegramConfig {
